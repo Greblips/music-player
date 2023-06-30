bef8998d27377d349dcf8eb6f6253b1d083c19fa
Evgeny <evgenspilberg@gmail.com>
2023-06-30 07:17:13 +0300

    add sprite svg

--- src/components/bar/bar-player-block/barPlayerBlockContent/barPlayerContentControls/barPlayerControls.jsx
+++ src/components/bar/bar-player-block/barPlayerBlockContent/barPlayerContentControls/barPlayerControls.jsx
@@ -1,29 +1,35 @@
-import { ReactComponent as Play } from "../../../../../img/icon/play.svg"
-import { ReactComponent as Prev } from "../../../../../img/icon/prev.svg"
-import { ReactComponent as Next } from "../../../../../img/icon/next.svg"
-import { ReactComponent as Repeat } from "../../../../../img/icon/repeat.svg"
-import { ReactComponent as Shuffle } from "../../../../../img/icon/shuffle.svg"
+import sprite from '../../../../../img/icon/sprite.svg';
 
 function PlayerControls() {
   return (
     <div className="player__controls">
       <div className="player__btn-prev">
-        <Prev className="player__btn-prev-svg" alt="prev" />
+        <svg className="player__btn-prev-svg" alt="prev">
+          <use xlinkHref={`${sprite}#icon-prev`} />
+        </svg>
       </div>
       <div className="player__btn-play _btn">
-        <Play className="player__btn-play-svg" alt="play" />
+        <svg className="player__btn-play-svg" alt="play">
+          <use xlinkHref={`${sprite}#icon-play`} />
+        </svg>
       </div>
       <div className="player__btn-next">
-        <Next className="player__btn-next-svg" alt="next" />
+        <svg className="player__btn-next-svg" alt="next">
+          <use xlinkHref={`${sprite}#icon-next`} />
+        </svg>
       </div>
       <div className="player__btn-repeat _btn-icon">
-        <Repeat className="player__btn-repeat-svg" alt="repeat" />
+        <svg className="player__btn-repeat-svg" alt="repeat">
+          <use xlinkHref={`${sprite}#icon-repeat`} />
+        </svg>
       </div>
       <div className="player__btn-shuffle _btn-icon">
-        <Shuffle className="player__btn-shuffle-svg" alt="shuffle" />
+        <svg className="player__btn-shuffle-svg" alt="shuffle">
+          <use xlinkHref={`${sprite}#icon-shuffle`} />
+        </svg>
       </div>
     </div>
-  )
+  );
 }
 
-export default PlayerControls
+export default PlayerControls;
